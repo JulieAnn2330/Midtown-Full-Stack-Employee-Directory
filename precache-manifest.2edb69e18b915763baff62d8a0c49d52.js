f1f5853fd76698e9b30a02b256b069bf8021ea17
Julie Schaub <julie.schaub@hotmail.com>
2020-10-23 11:11:02 -0500

    Updates

---
+++ precache-manifest.2edb69e18b915763baff62d8a0c49d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682ec72b599b5c0c274679e96bcfbc4b",
+    "url": "/Midtown-Full-Stack-Employee-Directory/index.html"
+  },
+  {
+    "revision": "a9b5b9994287e6e7e031",
+    "url": "/Midtown-Full-Stack-Employee-Directory/static/css/main.5144ba18.chunk.css"
+  },
+  {
+    "revision": "65199d8a356d7e521cc8",
+    "url": "/Midtown-Full-Stack-Employee-Directory/static/js/2.754c059f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Midtown-Full-Stack-Employee-Directory/static/js/2.754c059f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b5b9994287e6e7e031",
+    "url": "/Midtown-Full-Stack-Employee-Directory/static/js/main.964f813f.chunk.js"
+  },
+  {
+    "revision": "fc131e46eb0c5525eacc",
+    "url": "/Midtown-Full-Stack-Employee-Directory/static/js/runtime-main.6f57e014.js"
+  }
+]);
