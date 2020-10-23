cfd3c0bed88e10b13d0f5a316afeed7449ed6d5d
Julie Schaub <julie.schaub@hotmail.com>
2020-10-23 10:30:45 -0500

    updated header and footer

--- employee-directory/src/components/Footer.js
+++ employee-directory/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-    return <div className="Footer"></div>
+    return <div className="Footer">Created by Julie Schaub, University of Kansas Full Stack Boot Camp, October 2020</div>
 }
 
 export default Footer;
--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -4,7 +4,7 @@ import Midtown from './Midtown-Logo-2.png'
 function Header() {
     return (
         <div className="header">
-            <h1>Midtown Full Stack Employee Directory</h1>
+            <h1>Midtown Full Stack Technologies Employee Directory</h1>
             <img className="logo" src= { Midtown } alt="Logo" />
         </div>
     );
